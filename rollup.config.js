d9b965e2995383f55c6a4e2065d7c5834d65d90a
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2025-02-12 14:16:34 +0600

    install rollup

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'cjs'
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript()
+  ]
+};
